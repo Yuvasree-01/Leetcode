0f89822fa94b490f9fa89bdd9454d557847f9738
Yuvasree-01 <121015977+Yuvasree-01@users.noreply.github.com>
2024-07-31 15:12:35 +0530

    Time: 5 ms (40.02%), Space: 45.1 MB (30.79%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -6,8 +6,8 @@ class Solution {
         String vowels ="AEIOUaeiou";
         while(start <end){
             
-            if((ch[start]=='A' || ch[start]=='E' || ch[start]=='I' || ch[start]=='O' ||ch[start]=='U' || ch[start]=='a' || ch[start]=='e'|| ch[start]=='i' || ch[start]=='o' ||ch[start]=='u' ) && (ch[end]=='A'|| ch[end]=='E'|| ch[end]=='I' || ch[end]=='O' ||ch[end]=='U' || ch[end]=='a' || ch[end]=='e'|| ch[end]=='i' || ch[end]=='o' ||ch[end]=='u' ) )
-            //if(vowels.indexOf(ch[start])<=0 && vowels.indexOf(ch[end])<=0)
+            //if((ch[start]=='A' || ch[start]=='E' || ch[start]=='I' || ch[start]=='O' ||ch[start]=='U' || ch[start]=='a' || ch[start]=='e'|| ch[start]=='i' || ch[start]=='o' ||ch[start]=='u' ) && (ch[end]=='A'|| ch[end]=='E'|| ch[end]=='I' || ch[end]=='O' ||ch[end]=='U' || ch[end]=='a' || ch[end]=='e'|| ch[end]=='i' || ch[end]=='o' ||ch[end]=='u' ) )
+            if(vowels.indexOf(ch[start])>=0 && vowels.indexOf(ch[end])>=0)
             {
                 char temp = ch[start];
                 ch[start] = ch[end];
@@ -15,14 +15,14 @@ class Solution {
                 start++;
                 end--;
             }
-            else if(!(ch[start]=='A' || ch[start]=='E' || ch[start]=='I' || ch[start]=='O' ||ch[start]=='U' || ch[start]=='a' || ch[start]=='e'|| ch[start]=='i' || ch[start]=='o' ||ch[start]=='u' ) )
-            //else if(vowels.indexOf(ch[start]) <0)
+            //else if(!(ch[start]=='A' || ch[start]=='E' || ch[start]=='I' || ch[start]=='O' ||ch[start]=='U' || ch[start]=='a' || ch[start]=='e'|| ch[start]=='i' || ch[start]=='o' ||ch[start]=='u' ) )
+            else if(vowels.indexOf(ch[start]) <0)
             {
                 start++;
             }
             
-            else if(!(ch[end]=='A'|| ch[end]=='E'|| ch[end]=='I' || ch[end]=='O' ||ch[end]=='U' || ch[end]=='a' || ch[end]=='e'|| ch[end]=='i' || ch[end]=='o' ||ch[end]=='u' ))
-            //else if(vowels.indexOf(ch[end]) <0)
+            //else if(!(ch[end]=='A'|| ch[end]=='E'|| ch[end]=='I' || ch[end]=='O' ||ch[end]=='U' || ch[end]=='a' || ch[end]=='e'|| ch[end]=='i' || ch[end]=='o' ||ch[end]=='u' ))
+            else if(vowels.indexOf(ch[end]) <0)
             {
                 end--;
             }
